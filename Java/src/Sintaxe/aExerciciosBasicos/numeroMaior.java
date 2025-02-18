ef139d23a94e596e3d005105e0fc60d6538f61aa
Samuel Souza <samuel.souzato@gmail.com>
2025-02-18 16:21:55 -0400

    dia 4 (exercicios)

---
+++ Java/src/Sintaxe/aExerciciosBasicos/numeroMaior.java
@@ -0,0 +1,37 @@
+package Sintaxe.aExerciciosBasicos;
+
+import java.util.Scanner;
+
+//Solicite três números e determine qual é o maior deles.
+
+public class numeroMaior {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int n1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int n2 = scanner.nextInt();
+
+        System.out.println("Digite o terceiro número: ");
+        int n3 = scanner.nextInt();
+
+        if(n1 >= n2 && n1 >= n3){
+            System.out.println("Os números digitados foram: " + n1 + ", " + n2 + " e " + n3 + ". O maior entre eles é: " + n1);
+
+        }else if (n2 >= n1 && n2 >= n3){
+            System.out.println("Os números digitados foram: " + n1 + "," + n2 + " e "+ n3 + ". O maior entre eles é: " + n2);
+            
+        }else
+        System.out.println("Os números digitados foram: " + n1 +", " + n2 + " e " + n3 + ". O maior entre eles é: " + n3);
+        
+
+
+
+
+
+
+    }
+}
